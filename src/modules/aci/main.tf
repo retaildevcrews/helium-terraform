8e5f3ec8c13aa226b743e92f2d86a0d70d3c91fb
Akinlolu Akindele <aakindel@microsoft.com>
2020-08-25 14:16:51 -0500

    issue #27 use latest image instead if debug. update branch ref from master to main

--- src/modules/aci/main.tf
+++ src/modules/aci/main.tf
@@ -40,7 +40,7 @@ resource "azurerm_container_group" helium-aci {
   container {
     name     = "${var.NAME}-webv-${each.key}"
     image    = "retaildevcrew/webvalidate:latest"
-    commands = ["dotnet", "../webvalidate.dll", "--server", "${var.NAME}", "--files", "${var.CONTAINER_FILE_NAME}", "--base-url", "https://raw.githubusercontent.com/retaildevcrews/${var.REPO}/master/TestFiles/", "--run-loop", "--sleep", "${each.value}", "--summary-minutes", "5", "--json-log", "--tag", "${each.key}"]
+    commands = ["dotnet", "../webvalidate.dll", "--server", "${var.NAME}", "--files", "${var.CONTAINER_FILE_NAME}", "--base-url", "https://raw.githubusercontent.com/retaildevcrews/${var.REPO}/main/TestFiles/", "--run-loop", "--sleep", "${each.value}", "--summary-minutes", "5", "--json-log", "--tag", "${each.key}"]
     cpu      = "0.5"
     memory   = "1.5"
 
