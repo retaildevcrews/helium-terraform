3eb88bf25fc89d1eb98683964d51e99ab777f375
Alfredo Hernandez <52010147+alfredoihernandez@users.noreply.github.com>
2020-08-26 15:48:41 -0500

    Fix sed errors in create-tf-vars.sh on macOS (#30)
    * chain sed commands to not use in place editing
    
    * global replaces not needed
    
    * regex not needed
    
    Co-authored-by: alfredoihernandez <a14hdz@gmail.com.com>

--- src/root/create-tf-vars.sh
+++ src/root/create-tf-vars.sh
@@ -30,37 +30,38 @@ fi
 
 # create terraform.tfvars and replace template values
 
+cat ../example.tfvars | \
 # replace name
-cat ../example.tfvars | sed "s/<<He_Name>>/$He_Name/g" > terraform.tfvars
+  sed "s|<<He_Name>>|$He_Name|" | \
 
 # replace location
-sed -i "s/<<He_Location>>/$He_Location/g" terraform.tfvars
+  sed "s|<<He_Location>>|$He_Location|" | \
 
 # replace repo
-sed -i "s/<<He_Repo>>/$He_Repo/g" terraform.tfvars
+  sed "s|<<He_Repo>>|$He_Repo|" | \
 
 # replace email
-sed -i "s/<<He_Email>>/$He_Email/g" terraform.tfvars
+  sed "s|<<He_Email>>|$He_Email|" | \
 
 # replace TF_TENANT_ID
-sed -i "s/<<HE_TENANT_ID>>/$(az account show -o tsv --query tenantId)/g" terraform.tfvars
+  sed "s|<<HE_TENANT_ID>>|$(az account show -o tsv --query tenantId)|" | \
 
 # replace TF_SUB_ID
-sed -i "s/<<HE_SUB_ID>>/$(az account show -o tsv --query id)/g" terraform.tfvars
+  sed "s|<<HE_SUB_ID>>|$(az account show -o tsv --query id)|" | \
 
 # create a service principal
 # replace TF_CLIENT_SECRET
-sed -i "s/<<HE_CLIENT_SECRET>>/$(az ad sp create-for-rbac -n http://${He_Name}-tf-sp --query password -o tsv)/g" terraform.tfvars
+  sed "s|<<HE_CLIENT_SECRET>>|$(az ad sp create-for-rbac -n http://${He_Name}-tf-sp --query password -o tsv)|" | \
 
 # replace TF_CLIENT_ID
-sed -i "s/<<HE_CLIENT_ID>>/$(az ad sp show --id http://${He_Name}-tf-sp --query appId -o tsv)/g" terraform.tfvars
+  sed "s|<<HE_CLIENT_ID>>|$(az ad sp show --id http://${He_Name}-tf-sp --query appId -o tsv)|" | \
 
 # create a service principal
 # replace ACR_SP_SECRET
-sed -i "s/<<HE_ACR_SP_SECRET>>/$(az ad sp create-for-rbac --skip-assignment -n http://${He_Name}-acr-sp --query password -o tsv)/g" terraform.tfvars
+  sed "s|<<HE_ACR_SP_SECRET>>|$(az ad sp create-for-rbac --skip-assignment -n http://${He_Name}-acr-sp --query password -o tsv)|" | \
 
 # replace ACR_SP_ID
-sed -i "s/<<HE_ACR_SP_ID>>/$(az ad sp show --id http://${He_Name}-acr-sp --query appId -o tsv)/g" terraform.tfvars
+  sed "s|<<HE_ACR_SP_ID>>|$(az ad sp show --id http://${He_Name}-acr-sp --query appId -o tsv)|" > terraform.tfvars
 
 # validate the substitutions
 cat terraform.tfvars
