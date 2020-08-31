6b9671a9564f9d710f9968acd33abf457e846c04
Gerardo Ortega <gortega@microsoft.com>
2020-08-31 16:44:27 -0500

    issue #36 - Updated terraform to fix Access Policy by creating multiple azurerm_key_vault resources

--- src/modules/webapp/main_kv.tf
+++ src/modules/webapp/main_kv.tf
@@ -9,12 +9,14 @@ resource azurerm_key_vault kv {
   enabled_for_deployment          = false
   enabled_for_disk_encryption     = false
   enabled_for_template_deployment = false
+}
 
-  access_policy {
-    tenant_id = data.azurerm_client_config.current.tenant_id
-    object_id = data.azurerm_client_config.current.object_id
+resource "azurerm_key_vault_access_policy" "terraform-sp" {
+  key_vault_id = azurerm_key_vault.kv.id
+  tenant_id = data.azurerm_client_config.current.tenant_id
+  object_id = data.azurerm_client_config.current.object_id
 
-    secret_permissions = [
+  secret_permissions = [
       "Get",
       "List",
       "Set",
@@ -23,8 +25,10 @@ resource azurerm_key_vault kv {
       "Backup",
       "Restore"
     ]
-  }
+
 }
+
+
 
 resource "azurerm_key_vault_access_policy" "web_app" {
   key_vault_id = azurerm_key_vault.kv.id
