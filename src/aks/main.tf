c5fc239ea20fdd590d63bfbabc5b21c56aadb097
Siva Mullapudi <sivamu@microsoft.com>
2020-09-03 15:54:37 -0500

    Creating public aks cluster

--- src/aks/main.tf
+++ src/aks/main.tf
@@ -21,3 +21,24 @@ resource "azurerm_resource_group" "helium-app" {
   name     = "${var.NAME}-rg-app"
   location = var.LOCATION
 }
+
+resource "azurerm_kubernetes_cluster" "helium-aks" {
+  name                = "${var.NAME}-aks"
+  location            = var.LOCATION
+  resource_group_name = "${var.NAME}-rg-app"
+  dns_prefix          = "${var.NAME}-aks"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 3
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  role_based_access_control {
+    enabled = true
+  }
+}
