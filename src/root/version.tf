2aa2a487975cd20dc50d0da4efb34b9bad936fef
ritesh-modi <ritesh-modi@outlook.com>
2020-11-17 14:19:54 +0530

    rectified formatting issues

--- src/root/version.tf
+++ src/root/version.tf
@@ -1,15 +1,15 @@
 terraform {
   required_providers {
     azuread = {
-      source = "hashicorp/azuread"
+      source  = "hashicorp/azuread"
       version = "~>1.0"
     }
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "~>2.0"
     }
     docker = {
-      source = "terraform-providers/docker"
+      source  = "terraform-providers/docker"
       version = "~>2.7"
     }
     null = {
